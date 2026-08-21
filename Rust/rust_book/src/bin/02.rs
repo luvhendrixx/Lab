def172574856041758456da7f1d7900cc78a5583
Bond <Silverstonejr@gmail.com>
2026-08-21 14:36:59 +0300

    Understanding for loops

--- Rust/rust_book/src/bin/02.rs
+++ Rust/rust_book/src/bin/02.rs
@@ -1,22 +1,17 @@
-fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        // giving the loop a name
-        // the ' is rust syntax for naming loops (loop label)
-        println!("Count = {count}");
-        let mut remaining = 10;
+// 1km = 1000m
+fn km_to_m(km: u32) -> u32 {
+    km * 1000
+}
 
-        loop {
-            println!("Remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up; // telling rust what loop we want to break out of
-            }
-            remaining -= 1;
-        }
-        count += 1;
-    }
-    println!("End count = {count}");
+fn m_to_km(m: u32) -> u32 {
+    m / 1000
+}
+
+fn main() {
+    let km: u32 = 1;
+    let m = km_to_m(km);
+    println!("1km to m is {}", m);
+
+    let m_to_km = m_to_km(m);
+    println!("1000m to km is {}", m_to_km);
 }
