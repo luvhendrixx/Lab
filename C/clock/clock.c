f0992f245cc97187c102d37bc8031ae298fa8273
Bond <Silverstonejr@gmail.com>
2026-09-07 22:51:10 +0300

    chore: cleaned up and improved some aesthetics as well as logic

--- C/clock/clock.c
+++ C/clock/clock.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdbool.h>
-#include <unistd.h> // unix std -> provides a sleep func
+#include <unistd.h> // unix std -> provides the sleep func
 
-void main() {
+int main(void) {
     time_t rawtime = 0;
     // we give it NULL cause we haven't found an address to give to it yet
     struct tm *pTime = NULL; // if we don't assing a value to a struct, it ought to hold garbage value...so we assign NULL meaning it should hold no value
-    bool isRunning = true;
+    bool isRunning = true; // keeps the loop running indefinetly
 
     printf("DIGITAL CLOCK\n");
 
     while(isRunning) { // while is running == true...exe the code in the {}
-        time(&rawtime);
+        time(&rawtime); // grabs the raw sys time in seconds and overwrites the 0 we set above
 
+        // takes the raw sec and auto converts them to my local timezone as well as breaks...
+        // them into components (hours, min, seconds)...
+        // and saves the organized data inside our pTime struct
         pTime = localtime(&rawtime); // now we can give pTime an address which "overwrites" the NULL
 
         // the ? is just shorthand if/else logic....
         // if the condition is true, it picks "PM"..
         // otherwise, "AM"
+        // think of the pTime->tm_hour as..pulling a card (tm_hour) from a full pack of cards (pTime)
+        // if u decided to not use the -> op, you'd have to write (*pTime).tm_hour
         char *am_pm = (pTime->tm_hour >= 12) ? "PM" : "AM";
 
         int hour12 = pTime->tm_hour % 12;
@@ -31,6 +36,7 @@ void main() {
         fflush(stdout);
         // we pause a sec cuz of CPU efficiency (the PC will display data at lightning speed and throtlle comp resources)
         // and for human readability (how can you read stuff going at the speed of light?)
-        sleep(1); // DO NOT REMOVE AND RUN THE PROGRAMME WITHOUT THIS..trust :-()
+        sleep(1); // DO NOT REMOVE AND RUN THE PROGRAMME WITHOUT THIS UNLESS YOU WANT TO CRASH YOUR PC!!..trust :-()
     }
+    return 0;
 }
